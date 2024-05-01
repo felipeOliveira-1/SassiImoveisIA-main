f2f8c2d7a645a60fa516a7cf3f2efa5bc6bce099
felipeOliveira-1 <contato@felipesilva.tec>
2024-05-01 00:58:34 -0300

    commit

--- main.py
+++ main.py
@@ -1,25 +1,42 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 import os
 import json
 
 app = FastAPI()
 
-# Base directory inside your project where the 'imoveis' folder is located.
+class ContentItem(BaseModel):
+    id_imovel: int
+    numero_chave: str
+    tipo_imovel: str
+    finalidade_imovel: str
+    transacao: str
+    valor_locacao: float
+    valor_iptu: float
+    area: float
+    valor_condominio: float
+    descricao: str
+    Detalhes: list
+    bairro: str
+    cidade: str
+    cep: str
+    sala: int
+    quarto: int
+    banheiro: int
+    cozinha: int
+    vagas_garagem: int
+    destaque: str
+    fotos: list
+
 base_dir = "sassiImoveisIA-main"
 
 @app.get("/{transaction_type}/{dir_name}/{file_name}")
 async def read_imovel(transaction_type: str, dir_name: str, file_name: str):
-    # Construct the full path to the file
     full_path = os.path.join(base_dir, transaction_type, dir_name, file_name)
-
-    # Check if the file exists, if not, return a 404 error
     if not os.path.exists(full_path):
         return {"error": "File not found", "path": full_path}
 
-    # Open and read the JSON file, then return its contents
     with open(full_path, 'r', encoding='utf-8') as file:
         data = json.load(file)
-    return data
-
-
-
+        content_item = ContentItem(**data)
+        return content_item.dict()
